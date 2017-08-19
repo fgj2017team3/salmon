08038f021017295c465e88b166fafa5b7259834e
YukiOgino <YukiOgino@oginotakeshiki-no-MacBook-Air.local>
2017-08-19 17:19:03 +0900

    活力回復値を実装

--- Assets/ogino/Script/ResidentsScript.cs
+++ Assets/ogino/Script/ResidentsScript.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 /**
  * 住民のスクリプト
+ * 
+ * @author ogino
  */
 public class ResidentsScript : MonoBehaviour {
 
 	/** 当たり判定の半径 */
 	public float radius = 10.0f;
+
+	/** 活力回復値 */
+	public int resilientRecoverNum = 10;
+
+	/** あたり済みフラグ */
+	bool isHit = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,12 +25,18 @@ public class ResidentsScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Collider2D hitColliders = Physics2D.OverlapCircle(transform.localPosition, radius);
-		print(transform.localPosition);
-		if (hitColliders) {
-			if (hitColliders.gameObject.GetComponent<Salmon>()) {
-				Salmon salmon = hitColliders.gameObject.GetComponent<Salmon>();
-				print("活力アップ");
+
+		if (!isHit) {
+			// 何かコリジョンが当たっていないか、球状にレイを飛ばして判定
+			Collider2D hitColliders = Physics2D.OverlapCircle(transform.localPosition, radius);
+			// 何かコリジョンをもつコンポーネントがヒットした場合
+			if (hitColliders) {
+				// サーモンのスクリプトを持っているか？
+				if (hitColliders.gameObject.GetComponent<Salmon>()) {
+					Salmon salmon = hitColliders.gameObject.GetComponent<Salmon>();
+					salmon.resilient += resilientRecoverNum;
+					isHit = true;
+				}
 			}
 		}
 	}
